7a8acd6253e9196cbe946600c9049113a0e2882d
Lucas Bertoloto dos Santos <lucasbdoss@hotmail.com>
2021-10-27 17:42:14 -0300

    Footer frist version was completed

--- src/app/footer/footer.component.ts
+++ src/app/footer/footer.component.ts
@@ -1,4 +1,11 @@
 import { Component, OnInit } from '@angular/core';
+import { faAngular } from '@fortawesome/free-brands-svg-icons';
+import { faBootstrap } from '@fortawesome/free-brands-svg-icons';
+import { faLeaf } from '@fortawesome/free-solid-svg-icons';
+import { faEnvira } from '@fortawesome/free-brands-svg-icons';
+import { faLinkedin } from '@fortawesome/free-brands-svg-icons';
+import { faGithub } from '@fortawesome/free-brands-svg-icons';
+import { faEnvelope } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-footer',
@@ -6,10 +13,20 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./footer.component.scss']
 })
 export class FooterComponent implements OnInit {
+  faLinkedin = faLinkedin;
+  faGithub = faGithub;
+  faAngular = faAngular;
+  faBootstrap = faBootstrap;
+  faLeaf = faLeaf;
+  faEnvira = faEnvira;
+  faEnvelope = faEnvelope;
 
-  constructor() { }
+  constructor() { 
+    
+  }
 
   ngOnInit(): void {
+    
   }
 
 }
