da3372d78054dd82b42328ff6268406920dfd342
Lucas Bertoloto dos Santos <lucasbdoss@hotmail.com>
2021-10-28 17:11:25 -0300

    The structure is ready to start the main Div

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { InputNameComponent } from './input-name/input-name.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', redirectTo: '/index', pathMatch: 'full' },
+  {path:'index', component: InputNameComponent} 
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
