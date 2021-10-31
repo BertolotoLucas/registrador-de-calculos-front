1e2bcbc9add36d0840e8bda888212757fb944a39
Lucas Bertoloto dos Santos <lucasbdoss@hotmail.com>
2021-10-31 17:58:11 -0300

    Added functions to insert digits on the screen

--- src/app/calculator/calculator.component.ts
+++ src/app/calculator/calculator.component.ts
@@ -1,15 +1,21 @@
 import { Component, OnInit } from '@angular/core';
-
 @Component({
   selector: 'app-calculator',
   templateUrl: './calculator.component.html',
   styleUrls: ['./calculator.component.scss']
 })
 export class CalculatorComponent implements OnInit {
+  screenValue = '';
+  buttonValue = '';
 
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  addDigtToScreen(event: Event){
+    this.buttonValue = (event.target as HTMLInputElement).textContent || '';
+    this.screenValue = this.screenValue.concat(this.buttonValue);
   }
 
 }
