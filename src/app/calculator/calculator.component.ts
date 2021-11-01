d6052c772090d341783cf517e34a9c416a76dcbc
Lucas Bertoloto dos Santos <lucasbdoss@hotmail.com>
2021-11-01 16:14:14 -0300

    Added function to send op and receive the result

--- src/app/calculator/calculator.component.ts
+++ src/app/calculator/calculator.component.ts
@@ -7,6 +7,9 @@ import { Component, OnInit } from '@angular/core';
 export class CalculatorComponent implements OnInit {
   screenValue = '';
   buttonValue = '';
+  operation = '';
+  lastNumber = '';
+  canErasable = true;
 
   constructor() { }
 
@@ -14,7 +17,11 @@ export class CalculatorComponent implements OnInit {
   }
 
   addDigtToScreen(event: Event){
-    this.buttonValue = (event.target as HTMLInputElement).textContent || '';
+    if(this.canErasable){
+      this.canErasable = false;
+      this.clearTheScreen();
+    }
+    this.getButtonValue(event);
     this.screenValue = this.screenValue.concat(this.buttonValue);
   }
 
@@ -29,7 +36,65 @@ export class CalculatorComponent implements OnInit {
         this.screenValue = this.screenValue.concat('.');
     } else 
         this.screenValue = this.screenValue.concat('0.');
-      
+  }
+  
+  addOperation(event:Event){
+    if(this.screenValue){
+      this.lastNumber = this.screenValue;
+      this.getButtonValue(event);
+      switch(this.buttonValue) { 
+        case '+': { 
+          this.operation = "sum"; 
+          break; 
+        } 
+        case '-': { 
+          this.operation = "minus"; 
+          break; 
+        } 
+        default: { 
+          this.operation = ''; 
+          break; 
+        } 
+      }
+      this.canErasable = true;
+    }
+    console.log("operation: "+this.operation+", last number: "+this.lastNumber);
   }
 
+  getResult(){
+    console.log(this.screenValue);
+    if (this.lastNumber && this.screenValue && this.operation){
+      console.log("sending to server: "+ this.operation
+        + ", last number: "+this.lastNumber 
+        +" screen value: " + this.screenValue );
+      const x1 = Number(this.lastNumber);
+      const x2 = Number(this.screenValue)
+      switch(this.operation) {
+        case 'sum':{
+          this.screenValue = (x1+x2).toString();
+          break;
+        }
+        case 'minus':{
+          this.screenValue = (x1-x2).toString();
+          break;
+        }
+        default:{
+          console.log("Operation is not supported!");
+          break;
+        }
+      }
+      this.clearCalculator();
+    }
+  }
+  
+  private clearCalculator() {
+    this.buttonValue = '';
+    this.operation = '';
+    this.lastNumber = '';
+    this.canErasable = true;
+  }
+
+  private getButtonValue(event: Event) {
+    this.buttonValue = (event.target as HTMLInputElement).textContent || '';
+  }
 }
