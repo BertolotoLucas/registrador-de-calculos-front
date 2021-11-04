6abc5b5ebff549d98bb5fd5a4a0a4095cf75685b
Lucas Bertoloto dos Santos <lucasbdoss@hotmail.com>
2021-11-04 13:32:53 -0300

    Created the first version of the class Calculo

---
+++ src/app/model/calculo.spec.ts
@@ -0,0 +1,7 @@
+import { Calculo } from './calculo';
+
+describe('Calculo', () => {
+  it('should create an instance', () => {
+    expect(new Calculo()).toBeTruthy();
+  });
+});
---
+++ src/app/model/calculo.ts
@@ -0,0 +1,6 @@
+export class Calculo {
+    id?: string;
+    nomePessoa?: string;
+    operacao?: string;
+    resultado?: number;
+}
