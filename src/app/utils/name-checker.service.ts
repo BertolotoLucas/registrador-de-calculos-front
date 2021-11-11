aae23d714cdee92a439d08ba00af73e87b4eecbb
Lucas Bertoloto dos Santos <lucasbdoss@hotmail.com>
2021-11-11 15:04:37 -0300

    Add the first version to check the name & redirect

--- src/app/utils/name-checker.service.ts
+++ src/app/utils/name-checker.service.ts
@@ -1,9 +1,16 @@
 import { Injectable } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Injectable({
   providedIn: 'root'
 })
 export class NameCheckerService {
+  check(name: string, router: Router) {
+    if (!name){
+      router.navigate(['']);
+      alert("Você foi redirecionado, pois precisa inserir o seu nome!");
+    };
+  }
 
   constructor() { }
 }
