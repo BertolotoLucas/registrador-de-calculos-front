8445b8a944fb6626812ded5ed8444acf4784a62f
Lucas Bertoloto dos Santos <lucasbdoss@hotmail.com>
2021-11-11 14:51:51 -0300

    Dont appers the table if dont have any data

--- src/app/calculations/calculations.component.ts
+++ src/app/calculations/calculations.component.ts
@@ -1,7 +1,12 @@
 import { Component, OnInit } from '@angular/core';
+import { Observable, Subject } from 'rxjs';
 import { Calculo } from '../model/calculo';
 import { CalculoService } from '../service/calculo.service';
 import { OrganizerService } from '../utils/organizer.service';
+import {
+  debounceTime, distinctUntilChanged, switchMap
+} from 'rxjs/operators';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-calculations',
@@ -9,22 +14,38 @@ import { OrganizerService } from '../utils/organizer.service';
   styleUrls: ['./calculations.component.scss']
 })
 export class CalculationsComponent implements OnInit {
+  name = '';
   calculations: Calculo[] = [];
+  private searchTerms = new Subject<string>();
 
-  constructor(private util:OrganizerService, private calculoService:CalculoService) { }
+  constructor(private util:OrganizerService, private calculoService:CalculoService, private router:Router) {}
 
   ngOnInit():void {
-    this.getCalculos();
+    this.name = history.state.data;   
+    //this.getCalculos();
   }
 
   ngAfterViewChecked(): void {
     this.util.organizeTheBlocks();
   }  	
 
+
+  search(term: string): void {
+    this.searchTerms.next(term);
+  }
+
   getCalculos(){
     this.calculoService.getCalculos().toPromise().then(
       calculations => this.calculations = calculations
     );
+  }
+
+  returnToCalculator(){
+    const routes: string[] = [];
+    if (this.name) {
+      routes.push("calculator");
+      this.router.navigate(routes,{state:{data:this.name}});
+    }
   }
 
 }
