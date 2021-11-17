9ad0b7b65a8569d3a127bcefd5d2e7875e274103
Lucas Bertoloto dos Santos <lucasbdoss@hotmail.com>
2021-11-16 23:31:03 -0300

    clientHeight to getBoundingClientRect().height

--- src/app/utils/organizer.service.ts
+++ src/app/utils/organizer.service.ts
@@ -12,21 +12,17 @@ export class OrganizerService {
     let main = document.querySelector("main");
     main!.style.paddingTop = ("0px");
     main!.style.paddingBottom = ("0px");
-    heightMain = main!.clientHeight;
+    heightMain = Math.ceil(main!.getBoundingClientRect().height);
     if(heightMain)
     if(heightMain>=0){ 
       let header = document.querySelector("header");
-      var heightNavbar = header?.clientHeight;
-    
-      
+      var heightHeader = Math.floor(header!.getBoundingClientRect().height);     
       let footer = document.querySelector("footer");
-      var heightFooter = footer?.clientHeight;
-      var totalHeight = Number(heightMain) + Number(heightNavbar) + Number(heightFooter);
-
-
+      var heightFooter = Math.floor(footer!.getBoundingClientRect().height);
+      var totalHeight = heightMain + heightHeader + heightFooter;
       if (totalHeight < vh) {
         var i = vh - totalHeight;
-        var i2 = Math.floor(i / 2);
+        var i2 = i / 2;
         main!.style.paddingTop = (i2.toString() + "px");
         main!.style.paddingBottom = (i2.toString() + "px");
 
